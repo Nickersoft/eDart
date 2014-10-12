5ac794cf01404a5a9c00c506d3a10faa0b0023ea
Tyler Nickerson <nickersoft@gmail.com>
2014-10-12 16:15:01 -0400

    Set up beta dummy page to prevent further site access

---
+++ beta/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>eDart</title>
+
+		<!--Some Meta Stuff-->
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="SHORTCUT ICON" href="/img/favicon.ico" />
+
+		<!--Import CSS-->
+		<link rel="stylesheet" type="text/css" media="screen" href="/fonts/Titillium/stylesheet.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="/fonts/Vegur/stylesheet.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="/lib/uikit-2.9.0/css/uikit.almost-flat.min.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="/lib/min/?g=css">
+
+	</head>
+	<body>
+		<div id="notfound_container">
+			<h1>Goodbye, Beta!</h1>
+			<p>
+				Hey there! We're busy dropping the beta from our name. In addition, we're also adding tons of cool new stuff for you to try out! 
+				We're talking UI revamps, improved security, stability, and so much more! We should be back shortly, so make sure to periodically
+				check to see where we're at.
+			</p>
+
+			<span class="uk-icon-smile-o"></span>
+		</div>
+
+	</body>
+</html>
